2df432e9b7ad8039c30a714233ad558eed815636
Dmytro <dmytro.ilhanaiev@gmail.com>
2018-10-01 04:09:41 +0300

    Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": ["@illwatchers"],
+    "parserOptions": {
+        "ecmaVersion": 2015
+    }
+}
